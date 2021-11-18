400166c6de615e6027af111946394422ec342f81
streetmarker <streetmarker@users.noreply.github.com>
2021-11-18 21:53:30 +0100

    add first spring app

---
+++ firstSpringApp/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Ctr+Alt+O to optimize code
+@SpringBootApplication
+//@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
